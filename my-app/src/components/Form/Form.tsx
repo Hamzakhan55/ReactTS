81e48b6badeb5d6f680a3baf5322ec135cc36283
Hamza khan <hamzakhan127109@gmail.com>
2025-02-09 19:40:35 +0500

    create strutrue of form

--- my-app/src/components/Form/Form.tsx
+++ my-app/src/components/Form/Form.tsx
@@ -1,8 +1,18 @@
-
+2
 export const Form = () => {
   return (
-    <div>Form</div>
-  )
+        <form>
+            <div className="mb-3">
+                <label htmlFor="name" className="form-label">Name</label>
+                <input type="text" className="form-control" />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="age" className="form-label">Age</label>
+                <input id= "age" type="number" className="form-control" />
+            </div>
+            <button className="btn btn-primary" type="submit">Submit</button>
+        </form>
+)
 }
 
 export default Form;

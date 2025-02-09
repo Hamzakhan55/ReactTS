41ab4baa5ae0eefb949ee1fd351f4b15916f8f6d
Hamza khan <hamzakhan127109@gmail.com>
2025-02-09 19:00:42 +0500

    initialize form (creating Form)

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -6,6 +6,7 @@ import Like from './components/Like/Like';
 import Message from './components/Message';
 import Nav from './components/NavCartConnet.ysx/Nav';
 import Cart from './components/NavCartConnet.ysx/Cart';
+import Form from './components/Form/Form';
 
 
 function App() {
@@ -13,11 +14,15 @@ function App() {
   const [alertVisible, setAlertVisibility] = useState(false)
   return(<>
   {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>}
-  <Button onClick={()=> setAlertVisibility(true)}>My Button</Button> <br /><br />
+  <Button onClick={()=> setAlertVisibility(true)}>My Button</Button> <br /><br /><br />
   <Like onClick={() => console.log("Heart button was clicked")}/>
+  <br /><br /><br />
   <Message/><Message/><Message/>
+  <br /><br /><br />
   <Nav cartItemsCount={cartItems.length} />
   <Cart cartItems={cartItems} onClear={() => setCartItems([])}/>
+    <br /><br /><br />
+    <Form/>
   </>
 )
 }

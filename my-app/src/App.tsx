ad5dd3656d1a74f3858198e96d39575dc5f1c9cf
Hamza khan <hamzakhan127109@gmail.com>
2025-02-10 23:20:45 +0500

    Build Expense List

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -7,11 +7,19 @@ import Message from './components/Message';
 import Nav from './components/NavCartConnet.ysx/Nav';
 import Cart from './components/NavCartConnet.ysx/Cart';
 import Form from './components/Form/Form';
+import ExpenseList from './Mini-P-Expense-Tracker/ExpenseList';
 
 
 function App() {
   const [cartItems, setCartItems] = useState(['Product 1', 'Product 2'])
   const [alertVisible, setAlertVisibility] = useState(false)
+  const [expenses, setExpenses] = useState([
+    {id: 1, description: 'aaa', amount: 10, category: 'Utilities'},
+    {id: 2, description: 'bbb', amount: 30, category: 'Utilities'},
+    {id: 3, description: 'ccc', amount: 20, category: 'Utilities'},
+    {id: 4, description: 'ddd', amount: 40, category: 'Utilities'},
+    {id: 5, description: 'eee', amount: 10, category: 'Utilities'},
+])
   return(<>
   {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>}
   <Button onClick={()=> setAlertVisibility(true)}>My Button</Button> <br /><br /><br />
@@ -23,6 +31,7 @@ function App() {
   <Cart cartItems={cartItems} onClear={() => setCartItems([])}/>
     <br /><br /><br />
     <Form/>
+    <ExpenseList expenses = {expenses} onDelete = {(id) => setExpenses(expenses.filter(e => e.id != id))}/>
   </>
 )
 }
--- my-app/src/Mini-P-Expense-Tracker/ExpenseList.tsx
+++ my-app/src/Mini-P-Expense-Tracker/ExpenseList.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
 
-const ExpenseList = () => {
+interface Expense {
+  id: number;
+  description: string;
+  amount: number;
+  category: string;
+}
+
+interface props {
+  expenses: Expense[];
+  onDelete: (id: number) => void;
+}
+
+const ExpenseList = ({expenses, onDelete}: props) => {
+  if(expenses.length == 0) return null;
   return (
     <table className = "table table-bordered">
       <thead>
@@ -11,6 +24,25 @@ const ExpenseList = () => {
           <th></th>
         </tr>
       </thead>
+      <tbody>
+        {expenses.map(expense => <tr key = {expense.id}>
+            <td>{expense.description}</td>
+            <td>{expense.amount}</td>
+            <td>{expense.category}</td>
+            <td>
+              <button className="btn btn-outline-danger" onClick={() => onDelete(expense.id)}>Delete</button>
+            </td>
+        </tr>
+        )}
+      </tbody>
+      <tfoot>
+        <tr>
+          <td>Total</td>
+          <td>${expenses.reduce((acc, expense) => expense.amount + acc, 0).toFixed(2)}</td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tfoot>
     </table>
   )
 }

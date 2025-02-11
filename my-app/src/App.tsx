3967f7f1a5c0dfcb62bad2563536c4299fd1bc7a
Hamza khan <hamzakhan127109@gmail.com>
2025-02-11 20:04:40 +0500

    Build Expense Filter

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -8,18 +8,26 @@ import Nav from './components/NavCartConnet.ysx/Nav';
 import Cart from './components/NavCartConnet.ysx/Cart';
 import Form from './components/Form/Form';
 import ExpenseList from './Mini-P-Expense-Tracker/ExpenseList';
+import ExpenseFilter from './Mini-P-Expense-Tracker/components/ExpenseFilter';
 
 
 function App() {
+
+  const [selectedCategory, setSelectedCategory] = useState('');
   const [cartItems, setCartItems] = useState(['Product 1', 'Product 2'])
   const [alertVisible, setAlertVisibility] = useState(false)
   const [expenses, setExpenses] = useState([
     {id: 1, description: 'aaa', amount: 10, category: 'Utilities'},
     {id: 2, description: 'bbb', amount: 30, category: 'Utilities'},
-    {id: 3, description: 'ccc', amount: 20, category: 'Utilities'},
+    {id: 3, description: 'ccc', amount: 20, category: 'Entertainment'},
     {id: 4, description: 'ddd', amount: 40, category: 'Utilities'},
-    {id: 5, description: 'eee', amount: 10, category: 'Utilities'},
+    {id: 5, description: 'fff', amount: 10, category: 'Groceries'},
+    {id: 6, description: 'ggg', amount: 10, category: 'Utilities'},
+    {id: 7, description: 'hhh', amount: 10, category: 'Groceries'},
+    {id: 8, description: 'iii', amount: 10, category: 'Entertainment'},
 ])
+
+const visibleExpenses = selectedCategory ? expenses.filter(e => e.category === selectedCategory) : expenses;
   return(<>
   {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>}
   <Button onClick={()=> setAlertVisibility(true)}>My Button</Button> <br /><br /><br />
@@ -31,7 +39,10 @@ function App() {
   <Cart cartItems={cartItems} onClear={() => setCartItems([])}/>
     <br /><br /><br />
     <Form/>
-    <ExpenseList expenses = {expenses} onDelete = {(id) => setExpenses(expenses.filter(e => e.id != id))}/>
+    <div className = "mb-3">
+    <ExpenseFilter onSelectCategory ={(category) => setSelectedCategory(category)}/> 
+    </div>
+    <ExpenseList expenses = {visibleExpenses} onDelete = {(id) => setExpenses(expenses.filter(e => e.id != id))}/>
   </>
 )
 }

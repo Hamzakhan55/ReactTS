4788556a69be8bcbeed77aa1f0ffd35c97005255
Hamza khan <hamzakhan127109@gmail.com>
2025-02-18 20:43:43 +0500

    Updating data

--- my-app-2/src/App.tsx
+++ my-app-2/src/App.tsx
@@ -53,6 +53,19 @@ const deleteUser = (user: User) => {
         setUsers(originalUsers);
       })
     }
+      const updateUser = (user: User) => {
+        const originalUsers = [...users];
+        const updatedUser = {...user, name: user.name + "!"};
+        setUsers(users.map((u: User) => u.id === user.id ? updatedUser : u));
+
+        axios.patch('https://jsonplaceholder.typicode.com/users/' + user.id, updatedUser)
+        .catch(err => {
+          setError(err.message);
+          setUsers(originalUsers);
+
+        })
+      }
+
 
   return (<>
     {error && <p className = "text-danger">{error}</p>}
@@ -62,7 +75,7 @@ const deleteUser = (user: User) => {
       {users.map(user => <li key={user.id} className = "list-group-item d-flex justify-content-between">
         {user.name}{" "} 
         <div>
-          <button className="btn btn-outline-secondary">Update</button>
+          <button className="btn btn-outline-secondary mx-1" onClick={() => updateUser(user)}>Update</button>
           <button className="btn btn-outline-danger" onClick = {() => deleteUser(user)}>Delete</button>
       </div>
       </li>)}

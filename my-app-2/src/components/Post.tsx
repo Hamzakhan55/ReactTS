69ed7ec3c43cdd6fbd14b72d423a3b8808421110
Hamza khan <hamzakhan127109@gmail.com>
2025-03-21 23:38:42 +0500

    Paginated Queries use

--- my-app-2/src/components/Post.tsx
+++ my-app-2/src/components/Post.tsx
@@ -4,27 +4,23 @@ import usePost from "../hooks/usePost";
 
 
 const Post = () => {
-    const [userId, setUserId] = useState<number | undefined>(undefined);
-    const { data: posts, error, isLoading } = usePost(userId);
+    const pageSize = 10;
+    const [page, setPage] = useState(1);
+    const { data: posts, error, isLoading } = usePost({ page, pageSize });
 
     if (isLoading) return <p>Loading...</p>
     if (error) return <p className="text-danger">{error.message}</p>
     return (
         <>
-            <select onChange={(event) => setUserId(parseInt(event.target.value))}
-                value={userId}
-                className="form-select mb-3 mt-5">
-                <option>Select the User</option>
-                <option value="1">User 1</option>
-                <option value="2">User 2</option>
-                <option value="3">User 3</option>
-            </select>
             <h1>Posts</h1>
-            <div>
+            <ul>
                 {posts?.map((post) => (
-                    <div key={post.id}>{post.title}</div>
+                    <li key={post.id}>{post.title}</li>
                 ))}
-            </div>
+            </ul>
+            <button
+                disabled={page === 1} onClick={() => setPage(page - 1)} className="btn btn-primary my-2 me-2" style={{ marginRight: '5px' }}>Previous</button>
+            <button onClick={() => setPage(page + 1)} className="btn btn-primary my-2 ms-2">Next</button>
         </>
     )
 }
--- my-app-2/src/hooks/usePost.ts
+++ my-app-2/src/hooks/usePost.ts
@@ -2,24 +2,30 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios'
 
 interface Posts {
-    userId: number,
-    id: number,
-    title: string,
-    body: string,
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
 }
 
-const usePost = (userId: number | undefined) => {
+interface PostQuery {
+    page: number;
+    pageSize: number;
+}
+
+const usePost = (query: PostQuery) => {
     const fetchPosts = () =>
         axios
             .get<Posts[]>('https://jsonplaceholder.typicode.com/posts', {
                 params: {
-                    userId
+                    _start: (query.page - 1) * query.pageSize,
+                    _limit: query.pageSize
                 }
             })
             .then(res => res.data)
 
     return useQuery<Posts[], Error>({
-        queryKey: userId ? ['users', userId, 'posts'] : ['posts'],
+        queryKey: ['posts', query],
         queryFn: fetchPosts,
         staleTime: 10 * 1000,
     })
